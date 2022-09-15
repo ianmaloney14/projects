b7f1e27ef09033d9a6bcc9ca16d75da896fdb599
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-09-14 17:10:52 -0700

    Update gui

--- xrpl_wallet_python/gui/gui.py
+++ xrpl_wallet_python/gui/gui.py
@@ -1,27 +1,108 @@
-from tkinter import *
+import tkinter
+import tkinter.messagebox
+import customtkinter
 
-root = Tk()
-root.title('XRP Wallet')
-# root.iconbitmap('C:/Users/ianmaloney/Desktop/code/projects/xrpl_wallet_python/gui/img/xrp-logo (1).ico')
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-# Creating a Label Widget
-myLabel = Label(root, text="Username")
 
-# Pack labels on screen
-myLabel.pack()
+class App(customtkinter.CTk):
 
-e = Entry(root, width=50)
-e.pack()
+    WIDTH = 1280
+    HEIGHT = 720
 
-# onClick funtction
-def myClick():
-    hello = "Hello " + e.get()
-    myLabel = Label(root, text=hello)
-    myLabel.pack()
+    def __init__(self):
+        super().__init__()
 
-# Button
-myButton = Button(root, text="Submit", padx=50, command=myClick)
-myButton.pack()
+        self.title("XRP Wallet")
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
 
-# Event Loop
-root.mainloop()
+        # ============ create two frames ============
+
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(master=self,
+                                                 width=180,
+                                                 corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+
+        # ============ frame_left ============
+
+        # configure grid layout (1x11)
+        self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+
+        self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
+                                              text="Menu",
+                                              text_font=("Roboto Medium", -16))  # font name and size in px
+        self.label_1.grid(row=1, column=0, pady=10, padx=10)
+
+        self.button_1 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="Portfolio",
+                                                command=self.button_event)
+        self.button_1.grid(row=2, column=0, pady=10, padx=20)
+
+        self.button_2 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="Account",
+                                                command=self.button_event)
+        self.button_2.grid(row=3, column=0, pady=10, padx=20)
+
+        self.button_3 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="Send",
+                                                command=self.button_event)
+        self.button_3.grid(row=4, column=0, pady=10, padx=20)
+
+        self.button_4 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="Recieve",
+                                                command=self.button_event)
+        self.button_4.grid(row=5, column=0, pady=10, padx=20)
+
+
+        self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="Appearance Mode:")
+        self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
+
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_left,
+                                                        values=["Light", "Dark", "System"],
+                                                        command=self.change_appearance_mode)
+        self.optionmenu_1.grid(row=10, column=0, pady=10, padx=20, sticky="w")
+
+        # ============ frame_info ============
+
+        # configure grid layout (1x1)
+        # self.frame_info.rowconfigure(0, weight=1)
+        # self.frame_info.columnconfigure(0, weight=1)
+
+        # self.label_info_1 = customtkinter.CTkLabel(master=self.frame_info,
+        #                                            text="CTkLabel: Lorem ipsum dolor sit,\n" +
+        #                                                 "amet consetetur sadipscing elitr,\n" +
+        #                                                 "sed diam nonumy eirmod tempor" ,
+        #                                            height=100,
+        #                                            corner_radius=6,  # <- custom corner radius
+        #                                            fg_color=("white", "gray38"),  # <- custom tuple-color
+        #                                            justify=tkinter.LEFT)
+        # self.label_info_1.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
+
+        # self.progressbar = customtkinter.CTkProgressBar(master=self.frame_info)
+        # self.progressbar.grid(row=1, column=0, sticky="ew", padx=15, pady=15)
+
+        # set default values
+        self.optionmenu_1.set("Dark")
+
+    def button_event(self):
+        print("Button pressed")
+
+    def change_appearance_mode(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def on_closing(self, event=0):
+        self.destroy()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
