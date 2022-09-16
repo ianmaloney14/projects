b8ac92c21eb710ecb107a4872a6d6237a644b71e
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-09-16 14:51:17 -0700

    first commit

--- django_blog/blog/my_blog/models.py
+++ django_blog/blog/my_blog/models.py
@@ -4,13 +4,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    slug = models.SlugField()
-    intro = models.TextField()
     body = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title + ' | ' + self.author
-
-    class Meta:
-        ordering = ['-date_added']
+        return self.title + ' | ' + str(self.author)
