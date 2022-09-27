6a142a646f3192ebda53daa5b3f07fd5a119d80c
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-09-26 17:29:33 -0700

    add slugify

--- django_blog/blog/my_blog/views.py
+++ django_blog/blog/my_blog/views.py
@@ -13,8 +13,8 @@ class HomeView(ListView):
     # ordering = ['-id']
 
 def CategoryView(request, categories):
-    category_posts = Post.objects.filter(category=categories)
-    return render(request, 'categories.html', {'categories': categories.title(), 'category_posts':category_posts})
+    category_posts = Post.objects.filter(category=categories.replace('-', ''))
+    return render(request, 'categories.html', {'categories': categories.title().replace('-', ''), 'category_posts':category_posts})
 
 class ArticleDetailView(DetailView):
     model = Post
