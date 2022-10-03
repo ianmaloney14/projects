98613089799b0316c61554eca87bfb970bfdf033
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-10-03 05:38:02 -0700

    categories dropdown menu

--- django_blog/blog/my_blog/urls.py
+++ django_blog/blog/my_blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import ArticleDetailView, HomeView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import ArticleDetailView, HomeView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
 
 urlpatterns = [
     # path('', views.home, name='home'),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
     path('category/<str:categories>/', CategoryView, name='category'),
+    path('category-list/', CategoryListView, name='category-list'),
 ]
--- django_blog/blog/my_blog/views.py
+++ django_blog/blog/my_blog/views.py
@@ -12,6 +12,18 @@ class HomeView(ListView):
     ordering = ['-post_date']
     # ordering = ['-id']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list': cat_menu_list})
+
+
 def CategoryView(request, categories):
     category_posts = Post.objects.filter(category=categories.replace(' ', '-'))
     return render(request, 'categories.html', {'categories': categories.title().replace('-', ' '), 'category_posts':category_posts})
@@ -19,6 +31,12 @@ def CategoryView(request, categories):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 class AddPostView(CreateView):
     model = Post
