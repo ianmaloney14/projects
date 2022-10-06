ebeb8d2143689729f50e802eb0e871d475403b7a
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-10-05 19:53:26 -0700

    add like post

--- django_blog/blog/my_blog/urls.py
+++ django_blog/blog/my_blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import ArticleDetailView, HomeView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
+from .views import ArticleDetailView, HomeView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, LikeView
 
 urlpatterns = [
     # path('', views.home, name='home'),
@@ -12,4 +12,5 @@ urlpatterns = [
     path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
     path('category/<str:categories>/', CategoryView, name='category'),
     path('category-list/', CategoryListView, name='category-list'),
+    path('like/<int:pk>', LikeView, name='like_post'),
 ]
